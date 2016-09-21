572d5d7711e000821cb6fc5d07b316b39249645a
vz415 <vdzaball@gmail.com>
2016-09-21 17:06:32 +0100

    clustering

---
+++ clusterin notes.R
@@ -0,0 +1,3 @@
+# Clustering notes
+Use a dendogram to see the progression of hierarchical clustering. It's like a tree diagram that eventually gets up to the
+point of convergence. Cool visualization of what's happening. What's the quntitative cutoff?
